86ba4463833b7c2f832c9d9c40fa56c7cb8d892c
github username <github email address>
2020-07-04 14:16:44 -0400

    added routes to user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,44 @@
 class UsersController < ApplicationController
+
+    def show
+        @user = User.find_by(id: params[:id])
+    end
+
+    def new
+        @user = User.new
+    end
+
+    def create
+        @user = User.new(user_params(:name, :password, :email, :username))
+        if @user.save
+          redirect_to: user_path(@user)
+        else
+            render: new
+        end
+    end
+
+    def edit
+        @user = User.find_by(id: params[:id])
+    end
+
+    def edit
+        @user = User.find_by(id: params[:id])
+      end
+    
+      def update
+        @user = User.find_by(id: params[:id])
+        @user.update(user_params)
+        if @user.save
+          redirect_to user_path(@user)
+        else
+          render :edit
+        end
+      end
+
+    private
+
+    def user_params(*args)
+        params.require(:user).permit(*args)
+    end
+
 end
