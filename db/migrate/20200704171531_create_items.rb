25e4155d204c32431aab5742ba0e3eb158b0bf17
github username <github email address>
2020-07-04 13:16:04 -0400

    Item resource generated

---
+++ db/migrate/20200704171531_create_items.rb
@@ -0,0 +1,10 @@
+class CreateItems < ActiveRecord::Migration[6.0]
+  def change
+    create_table :items do |t|
+      t.string :description
+      t.string :name
+
+      t.timestamps
+    end
+  end
+end
