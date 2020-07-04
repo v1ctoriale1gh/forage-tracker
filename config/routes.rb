c7befdaf9063c1b7f646b4da3124475c87cbca12
github username <github email address>
2020-07-04 15:50:46 -0400

    added more routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,12 @@ Rails.application.routes.draw do
   resources :harvests
   resources :items
   resources :users do
-    resources :expeditions
+    resources :expeditions do
+      resources :harvests
+    end
+    resources :items do
+      resources :harvests, only: [:show]
+    end
   end
   resources :expeditions
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
